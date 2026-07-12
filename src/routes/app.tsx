265ae2565db326b9027d4e043f0c91ba4da2f22f
aisarus <arielperseny@gmail.com>
2026-07-13 02:33:07 +0300

    feat: activate visual-source lifecycle cleanup

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -2,10 +2,12 @@ import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { AppShell } from "@/components/app-shell";
 import { MaterialIntakeQueueProvider } from "@/components/material-intake-queue";
 import { MaterialIntakeRouteLauncher } from "@/components/material-intake-route-launcher";
+import { VisualSourceLifecycle } from "@/components/visual-source-lifecycle";
 
 export const Route = createFileRoute("/app")({
   component: () => (
     <MaterialIntakeQueueProvider>
+      <VisualSourceLifecycle />
       <AppShell>
         <MaterialIntakeRouteLauncher />
         <Outlet />
