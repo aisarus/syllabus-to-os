094de6132dd4a0405ea510d5f1c1679dba7ecde9
aisarus <arielperseny@gmail.com>
2026-07-12 19:58:07 +0300

    feat: expose multi-file queue on materials route

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -1,11 +1,13 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { AppShell } from "@/components/app-shell";
 import { MaterialIntakeQueueProvider } from "@/components/material-intake-queue";
+import { MaterialIntakeRouteLauncher } from "@/components/material-intake-route-launcher";
 
 export const Route = createFileRoute("/app")({
   component: () => (
     <MaterialIntakeQueueProvider>
       <AppShell>
+        <MaterialIntakeRouteLauncher />
         <Outlet />
       </AppShell>
     </MaterialIntakeQueueProvider>
