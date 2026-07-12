81491676ef26392d025a307afc62de1658a71501
aisarus <arielperseny@gmail.com>
2026-07-12 19:53:14 +0300

    feat: mount global material intake queue

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -1,10 +1,13 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { AppShell } from "@/components/app-shell";
+import { MaterialIntakeQueueProvider } from "@/components/material-intake-queue";
 
 export const Route = createFileRoute("/app")({
   component: () => (
-    <AppShell>
-      <Outlet />
-    </AppShell>
+    <MaterialIntakeQueueProvider>
+      <AppShell>
+        <Outlet />
+      </AppShell>
+    </MaterialIntakeQueueProvider>
   ),
 });
